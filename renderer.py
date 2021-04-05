b7bf6a2bdf3d0d6874f43acd5c25c64c0de06ea2
Sourish Banerjee <sourish.banerjee2015@vit.ac.in>
2021-04-05 04:37:16 -0700

    Implement render() function

--- renderer.py
+++ renderer.py
@@ -1,13 +1,33 @@
+import numpy as np
+import math
+from PIL import Image
+from scene.ray import Ray
+
+
 class Renderer:
     def __init__(self, scene, max_depth):
         self.scene = scene
         self.max_depth = max_depth
 
     def render(self):
-        # TODO: Call ray trace for each pixel in image
-        pass
+        # Call ray trace for each pixel in image
+        cam = self.scene.cam
+        cam_to_screen = np.linalg.norm(cam.cam_to - cam.cam_from)
+        screen_width = 2 * math.tan(cam.fov / 2) * cam_to_screen
+        screen_height = screen_width * cam.height / cam.width
+        increment = screen_width / cam.width
+        top_left = cam.cam_to - ((screen_width / 2) * cam.u) - ((screen_height / 2) * cam.v)
 
-    def ray_trace(self, ray, scene, depth=0):
+        im = Image.new('RGB', (cam.width, cam.height))
+        for i in range(cam.width):
+            for j in range(cam.height):
+                point = top_left + (i * increment * cam.u) + (j * increment * cam.v)
+                color = self.ray_trace(Ray(cam.cam_from, point), self.max_depth)
+                im.putpixel(tuple(i, j), tuple(color))
+
+        return im
+
+    def ray_trace(self, ray, depth=0):
         # TODO: Implement recursively. Find nearest intersection for ray, compute reflected ray and call recursively
         # with depth += 1 until depth == max_depth or no intersection is found.
         pass
--- scene/camera.py
+++ scene/camera.py
@@ -1,6 +1,9 @@
 class Camera:
-    def __init__(self, pos, width, height, fov):
-        self.pos = pos
+    def __init__(self, cam_from, cam_to, u, v, fov, width, height):
+        self.cam_from = cam_from
+        self.cam_to = cam_to
+        self.u = u
+        self.v = v
         self.width = width
         self.height = height
         self.fov = fov
