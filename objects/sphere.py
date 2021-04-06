41339f75be7e0946461a7a8635f8b38279849ad2
David Lin <davxlin@gmail.com>
2021-04-06 13:34:27 -0700

    implemented sphere intersection

--- objects/sphere.py
+++ objects/sphere.py
@@ -10,7 +10,24 @@ class Sphere(Object):
 
     def get_intersection(self, ray):
         # TODO: Implement intersection math, returns distance from ray origin
-        pass
+        # xd, yd, zd = ray.dir
+        # x0, y0, z0 = ray.origin
+        # xc, yc, zc = self.center
+
+        # vectorized calculation
+        b = 2 * np.sum(ray.dir * (ray.origin - self.center))
+        c = np.square(ray.origin - self.center).sum() - np.square(self.radius)
+
+        # a = 1 since ray dir is normalized
+        a = 1
+        d = np.sqrt(np.square(b) - 4 * a * c)   # discriminant
+        if d < 0:
+            return None
+
+        t0 = (-b + d) / (2 * a)
+        t1 = (-b - d) / (2 * a)
+
+        return np.min(t0, t1)
 
     def get_normal(self, point):
         # TODO: Returns normalized surface normal at point
