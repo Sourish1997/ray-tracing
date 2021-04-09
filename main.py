7de69887f653df34ac994f77f4cd7b257c1be01c
David Lin <davxlin@gmail.com>
2021-04-08 17:06:45 -0700

    added JSON scene parser and gitignore

--- main.py
+++ main.py
@@ -7,33 +7,52 @@ from objects.cylinder import Cylinder
 from materials.base_material import BaseMaterial
 import numpy as np
 import math
+import json
 
 
-def main():
-    # Create a scene object
-    # TODO: Parse a JSON
-    width = 512
-    height = 512
-    cam_from = np.array([2, -4, -8])
-    cam_to = np.array([2, -3, -6])
+def parse_scene_json(scene_json):
+    """
+    Param: scene_file - json file of scene description
+    Returns: Scene obj to render
+    """
+
+    with open(scene_json, "r") as scene_des:
+        scene = json.loads(scene_des.read())["scene"]
+
+    # objs, lights, camera = scene["shapes"], scene["lights"], scene["camera"]
+    # camera
+    width, height = scene["camera"]["resolution"]
+    cam_from = np.array(scene["camera"]["from"])
+    cam_to = np.array(scene["camera"]["to"])
     u = np.array([2, 0, 0])
     v = np.array([0, 2, -1])
     fov = math.pi / 3
     cam = Camera(cam_from, cam_to, u, v, fov, width, height)
-    light = Light(np.array([1, 1, 1]), np.array([4, 4, -10]))
-    prop = {'col': np.array([1, 0, 0]), 'amb': 0.4, 'dif': 0.6, 'spec': 0.5, 'ref': 0.8}
-    prop_2 = {'col': np.array([0, 1, 0]), 'amb': 0.4, 'dif': 0.6, 'spec': 0.5, 'ref': 0.8}
-    prop_3 = {'col': np.array([0, 0, 1]), 'amb': 0.4, 'dif': 0.6, 'spec': 0.5, 'ref': 0.8}
-    mat = {'material': BaseMaterial(**prop)}
-    mat_2 = {'material': BaseMaterial(**prop_2)}
-    mat_3 = {'material': BaseMaterial(**prop_3)}
-    cyl_1 = Cylinder(np.array([2, 0, 4]), 2, 12, np.array([0, 1, 0]), **mat_3)
-    sph_1 = Sphere(np.array([0, 0, 0]), 2, **mat)
-    sph_2 = Sphere(np.array([3, 0, 0]), 1, **mat_2)
-    objects = [sph_1, sph_2, cyl_1]
-    scene = Scene(cam, light, objects)
+
+    # light (only support for point lights rn, requires refactoring of light.py)
+    # TODO: need to add support for multiple lights as well
+    light = Light(np.array(scene["lights"][0]["color"]), np.array(scene["lights"][0]["pos"]))
+
+    objects = []
+    for obj in scene["shapes"]:
+        prop = {'col': np.array(obj["material"]["Cs"]), 'amb': obj["material"]["Ka"], 'dif': obj["material"]["Kd"], 'spec': obj["material"]["Ks"], 'ref': 0.8}
+        mat = {'material': BaseMaterial(**prop)}
+        geom_params = obj["geomParams"]
+        if obj["geometry"] == "sphere":
+            scene_obj = Sphere(np.array(geom_params["center"]), geom_params["radius"], **mat)
+        elif obj["geometry"] == "cylinder":
+            scene_obj = Cylinder(np.array(geom_params["center"]), geom_params["radius"], geom_params["h"], geom_params["v"], **mat)
+        objects.append(scene_obj)
+    return Scene(cam, light, objects)
+
+
+def main():
+    # Create a scene object
+    scene = parse_scene("scene.json")
+
     # Create a renderer object with scene passed as param
     renderer = Renderer(scene, 3)
+
     # Call the renderer's render function
     im = renderer.render()
     im.save('img.png')
