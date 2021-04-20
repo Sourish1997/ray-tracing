692eb1faa1a3c101c2397c10e73128c71b94e1be
David Lin <davxlin@gmail.com>
2021-04-19 17:16:44 -0700

    changed base_material::get_color for multiple light sources

--- materials/base_material.py
+++ materials/base_material.py
@@ -10,14 +10,17 @@ class BaseMaterial(Material):
     # NOTE: 'light' is a list of 'Light' objects from multiple light sources
     # TODO: need to add support for multiple lights as well
     def get_color(self, point, normal, camera, light):
-        # Lambertian shading for Diffuse:
-        n_dot_l = np.dot(light[0].pos / np.linalg.norm(light[0].pos), normal)
+        c_rgb = np.zeros(3)
 
-        # specularity
-        r = -1 * light[0].pos / np.linalg.norm(light[0].pos) + 2 * n_dot_l * normal
-        h = (light[0].pos + np.array(camera.v2 - point)) / np.linalg.norm((light[0].pos + np.array(camera.v2 - point)))
-        n_dot_h = np.dot(h, normal)
+        for i in range(len(light)):
+            # Lambertian shading for Diffuse:
+            n_dot_l = np.dot(light[i].pos / np.linalg.norm(light[i].pos), normal)
 
-        c_r, c_g, c_b = light[0].col * self.col * (self.dif * max(0, n_dot_l) + (light[0].intensity * self.spec * pow(max(0, n_dot_h), self.n)))
+            # specularity
+            r = -1 * light[i].pos / np.linalg.norm(light[i].pos) + 2 * n_dot_l * normal
+            h = (light[i].pos + np.array(camera.v2 - point)) / np.linalg.norm((light[i].pos + np.array(camera.v2 - point)))
+            n_dot_h = np.dot(h, normal)
 
-        return np.array([c_r, c_g, c_b])
+            c_rgb += light[i].col * self.col * (self.dif * max(0, n_dot_l) + (light[i].intensity * self.spec * pow(max(0, n_dot_h), self.n)))
+
+        return c_rgb
