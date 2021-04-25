0f65dc5c5af49d49342bbc004e9488de1cfba570
Harsha Rayudu <hrayudu@usc.edu>
2021-04-24 19:27:13 -0700

    adding triangle

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from scene.scene import Scene
 from renderer import Renderer
 from objects.sphere import Sphere
 from objects.cylinder import Cylinder
+from objects.plane import Plane
+from objects.triangle import Triangle
+# from objects.cone import Cone
 from materials.base_material import BaseMaterial
 import numpy as np
 import math
@@ -43,20 +46,26 @@ def parse_scene_json(scene_json):
             scene_obj = Sphere(np.array(geom_params["center"]), geom_params["radius"], **mat)
         elif obj["geometry"] == "cylinder":
             scene_obj = Cylinder(np.array(geom_params["center"]), geom_params["radius"], geom_params["h"], geom_params["v"], **mat)
+        elif obj["geometry"] == "plane":
+            scene_obj = Plane(np.array(geom_params["point0"]), np.array(geom_params["normal"]), **mat)
+        elif obj["geometry"] == "triangle":
+            scene_obj = Triangle(np.array(geom_params["v0"]), np.array(geom_params["v1"]), np.array(geom_params["v2"]), **mat)
+        # elif obj["geometry"] == "cone":
+        #     scene_obj = Cone(np.array(geom_params["pA"]), np.array(geom_params["p1"]), geom_params["r1"], **mat)
         objects.append(scene_obj)
     return Scene(cam, light, objects)
 
 
 def main():
     # Create a scene object
-    scene = parse_scene_json("scene.json")
+    scene = parse_scene_json("scene1.json")
 
     # Create a renderer object with scene passed as param
     renderer = Renderer(scene, 3)
 
     # Call the renderer"s render function
     im = renderer.render()
-    im.save("img.png")
+    im.save("img1.png")
 
 
 if __name__ == "__main__":
